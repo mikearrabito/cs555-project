9014406aca6f66de67cab457a70500ced67ff316
Mark Paris <58420689+markparis1@users.noreply.github.com>
2021-03-04 14:08:39 -0500

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,7 +1,32 @@
 class Person:
 
-    def __init__(self, id: str, name: str):
+    def __init__(self):
 
+    
+    def set_ID(self, id: str):
         self.id = id
+    
+    def set_name(self, name: str):
         self.name = name
+
+    def set_gender(self, gender: str):
+        self.gender = gender
+
+    def set_birthday(self, birthday: str):
+        self.birthday = birthday
+
+    def set_is_alive(self, isAlive: bool):
+        self.is_alive = is_alive
+    
+    def set_death(self, death: str):
+        self.death = death
+    
+    def set_is_child(self, is_child: bool):
+        self.is_child = is_child
+    
+    def set_is_spouse(self, is_spouse):
+        self.is_spouse = is_spouse
+
+    def set_fam(self, fam: str):
+        self.fam = fam
     
