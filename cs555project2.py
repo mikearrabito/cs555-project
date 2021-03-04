6ecbb033cb7ff345dc4202eab00f9282ea867e32
Michael Arrabito <mikearrabito@gmail.com>
2021-03-03 22:04:26 -0500

    initial

---
+++ cs555project2.py
@@ -0,0 +1,35 @@
+# Michael Arrabito CS555 Project 2
+import os
+import sys
+
+gedcom_file_path = os.path.join(os.getcwd(), 'family.ged')
+
+f = open(gedcom_file_path)
+
+
+def is_tag_valid(tag):
+    set_of_valid_tags = {'INDI', 'NAME', "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL",
+                         "DIV", "DATE", "HEAD", "TRLR", "NOTE"}
+    return tag in set_of_valid_tags
+
+
+for line in f.readlines():
+    line = line.rstrip()
+    print("--> " + line)
+    split = line.split(" ", 2)
+    level = split[0]
+    tag = split[1]
+    rest = ""
+    if len(split) > 2:
+        rest = split[2]
+        if rest == 'FAM' or rest == 'INDI':
+            tag = rest  # exception to tag being second value in line
+            rest = split[1]  # print the id at the end of our output string
+    valid = is_tag_valid(tag)
+    if valid:
+        valid = 'Y'
+    else:
+        valid = 'N'
+
+    print("<-- " + level + "|" + tag + "|" + valid + "|" + rest)
+
