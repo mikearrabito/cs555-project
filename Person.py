67d4971a7988edc449188c9709d6544997efaf93
Jenal09 <55665596+Jenal09@users.noreply.github.com>
2021-03-18 13:11:47 -0400

    Update Person.py

--- Person.py
+++ Person.py
@@ -47,7 +47,26 @@ class Person:
                 return
         age = today.year - birthday_date.year 
         self.age = age
-    
+        
+     """Marriage should occur before death of either spouse"""
+
+
+    def marriage_before_death(individual, family):
+        for fam in family.values():
+            married = fam._marriage_date
+            for indi in individual.values():
+                if fam._husband_id == indi._individual_id and indi._death_date != 'NA'and married!= "NA":
+                    if indi._death_date < married:
+                        yield f"Marriage date Line: {fam._line_numbers.get('date').get('marriage')}" \
+                            f"\nDeath of Husband date Line: {indi._line_numbers.get('date').get('death')}\n" \
+                            f"The family {fam._family_id} has a death of husband {fam._husband_id} before the marriage date."
+                elif fam._wife_id == indi._individual_id and indi._death_date != 'NA' and married!= "NA":
+                    if indi._death_date < married:
+                        yield f"Marriage date Line: {fam._line_numbers.get('date').get('marriage')}" \
+                            f"\nDeath of wife date Line: {indi._line_numbers.get('date').get('death')}\n" \
+                            f"The family {fam._family_id} has a death of wife {fam._wife_id} before the marriage date."
+                            
+
     def set_marriage_date(self, date: str):
         if datetime.datetime.strptime(date, "%d %b %Y") > datetime.datetime.strptime(self.birthday, "%d %b %Y"):
             if self.divorce_date:
