2fa83dbcfae2c8a472e99b986c56e0ad45f4d20a
Mark Paris <58420689+markparis1@users.noreply.github.com>
2021-03-18 14:25:11 -0400

    fixed

--- mp_tests.py
+++ mp_tests.py
@@ -10,7 +10,7 @@ class PersonTest(unittest.TestCase):
         person1 = Person()
         person1.set_birthday("10 JUN 2020")
         person1.set_death("10 MAY 2020")
-        self.assertEquals(person1.birthday, 'Invalid date')
+        self.assertEqual(person1.death, 'N/A')
         return
 
     def test_marriage_before_divorce(self):
