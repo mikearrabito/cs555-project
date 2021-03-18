4fbfbf7243054ca526ed3999806931ddb988e02b
Mark Paris <58420689+markparis1@users.noreply.github.com>
2021-03-18 14:23:43 -0400

    tried fixing tests

--- mp_tests.py
+++ mp_tests.py
@@ -3,18 +3,19 @@ import unittest
 
 class PersonTest(unittest.TestCase):
 
-    def test_is_date_valid(self) -> None:
+    def test_is_date_valid(self):
         self.assertTrue(Person.is_date_valid("3 MAR 2021"))
 
     def test_birth_before_death(self):
         person1 = Person()
-        person1.set_death("10 MAY 2020")
         person1.set_birthday("10 JUN 2020")
+        person1.set_death("10 MAY 2020")
         self.assertEquals(person1.birthday, 'Invalid date')
         return
 
     def test_marriage_before_divorce(self):
         person1 = Person()
+        person1.set_birthday("1 JAN 1997")
         person1.set_marriage_date("10 MAY 2020")
         person1.set_divorce_date("10 JUN 2020")
         self.assertEquals(person1.divorce_date, "10 JUN 2020")
